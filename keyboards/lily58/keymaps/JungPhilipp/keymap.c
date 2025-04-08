bd1e7a7b49895a9465cf1d9ba29a02da18a90bb1
Philipp Jung <philipp.jung@mailbox.org>
2025-04-08 12:03:42 +0200

    Enable media keys and fixup

--- keyboards/lily58/keymaps/JungPhilipp/keymap.c
+++ keyboards/lily58/keymaps/JungPhilipp/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_MOV] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, KC_HOME, KC_DEL , KC_UP  , KC_BSPC, KC_PGUP,                   KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
+  _______, KC_HOME, KC_BSPC, KC_UP  , KC_DEL,  KC_PGUP,                   KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
   _______, KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN ,                  KC_PSLS, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PCMM,
   _______, KC_INS , _______, _______, _______, _______, _______, _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PDOT, KC_PENT,
                              _______, _______, _______, _______, _______,  _______, _______, _______
