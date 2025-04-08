249e6db7746466bdad5a9286d5fa971c0b8566f6
Philipp Jung <philipp.jung@mailbox.org>
2025-04-08 11:46:41 +0200

    Update right MOV layer

--- keyboards/lily58/keymaps/JungPhilipp/keymap.c
+++ keyboards/lily58/keymaps/JungPhilipp/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |RShift|
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  '   |
+ * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RCTRL |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |  MOV | /Space  /       \Enter \  |  SYM |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -28,18 +28,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT,
-  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
                         KC_LALT, KC_LGUI, MO(_MOV), KC_SPC, KC_ENT, MO(_SYM), KC_BSPC, KC_RGUI
 ),
 /* MOVEMENT
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |PgUp  |BackSp|  Up  | Del  | Home |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |      | Home |BackSp|  Up  | Del  |Pg  Up|                    |   *  |   7  |   8  |   9  |  -   |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |PgDown| Left | Down |Right | End  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   ~  |
+ * |      | End  | Left | Down |Right |PgDown|-------.    ,-------|   /  |   4  |   5  |   6  |  +   |   ,  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |      |Insert|      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
+ * |      |Insert|      |      |      |      |-------|    |-------|   0  |   1  |   2  |   3  |  .   |Enter |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |  MOV | /Space  /       \Enter \  | SYM |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_MOV] = LAYOUT(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, KC_PGUP, KC_DEL , KC_UP  , KC_BSPC, KC_HOME,                   _______, _______, _______,_______, _______, _______,
-  KC_GRV , KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_END ,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-  _______, KC_INS , _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+  _______, KC_HOME, KC_DEL , KC_UP  , KC_BSPC, KC_PGUP,                   KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
+  _______, KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN ,                  KC_PSLS, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PCMM,
+  _______, KC_INS , _______, _______, _______, _______, _______, _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PDOT, KC_PENT,
                              _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 /* SYMBOLS
